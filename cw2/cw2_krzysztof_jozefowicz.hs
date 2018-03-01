8522a3b8e956e066b9b065c3aa56d0fb43ba367e
Krzysztof Józefowicz <s416085@pc01b.labs.wmi.amu.edu.pl>
2018-03-01 16:08:32 +0100

    zad 3

--- cw2/cw2_krzysztof_jozefowicz.hs
+++ cw2/cw2_krzysztof_jozefowicz.hs
@@ -1,6 +1,24 @@
 -- 1)
+ -- a)
 add_head list el = el : list
 
+ -- b)
 add_second list el = head list : el : tail list
 
+ -- c)
 add_tail list el = list ++ [el]
+
+-- 2)
+ -- a)
+second_element list = list !! 1
+
+ -- b)
+third_element list = list !! 2
+
+ -- c)
+sec_to_last list = list !! (length list - 2)
+
+-- 3)
+custom_reverse [] mynew = mynew
+custom_reverse myhead mynew = custom_reverse (tail myhead) ([head myhead] ++ mynew)
+custom_reverse_start list = custom_reverse list []
